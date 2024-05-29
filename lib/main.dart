b4d30c7609ae413653e406bc0b146b2bfeed2152
sanjiv576 <shresthasanjiv576@gmail.com>
2024-05-29 11:37:48 +0545

    add your public key

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'view/app.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
+// void main() => runApp(const ProviderScope(child: App()));
+
+// Khalti payment integration // Step 3
+Future main() async {
+  // reading .env file
+  await dotenv.load(fileName: '.env');
+
+  runApp(ProviderScope(child: App()));
+}
